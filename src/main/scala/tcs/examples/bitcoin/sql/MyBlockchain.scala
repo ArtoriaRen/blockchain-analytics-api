d4134886733b13291298c8bda6f5a2c7ce64e14b
l27ren <l27ren@uwaterloo.ca>
2018-03-06 10:46:41 -0500

    Changed rpcuser and rpcpassword

--- src/main/scala/tcs/examples/bitcoin/sql/MyBlockchain.scala
+++ src/main/scala/tcs/examples/bitcoin/sql/MyBlockchain.scala
@@ -14,8 +14,8 @@ import tcs.utils.DateConverter.convertDate
 object MyBlockchain{
   def main(args: Array[String]): Unit ={
 
-    val blockchain = BlockchainLib.getBitcoinBlockchain(new BitcoinSettings("user", "password", "8332", MainNet))
-    val mySQL = new DatabaseSettings("myblockchain", MySQL, "user", "password")
+    val blockchain = BlockchainLib.getBitcoinBlockchain(new BitcoinSettings("l27ren", "UofW2016", "8332", MainNet))
+    val mySQL = new DatabaseSettings("test", MySQL, "root", "UofW2016")
 
     val startTime = System.currentTimeMillis()/1000
 
@@ -24,47 +24,50 @@ object MyBlockchain{
         txid int(10) unsigned auto_increment not null primary key,
         transactionHash varchar(256) not null,
         blockHash varchar(256) not null,
-        timestamp TIMESTAMP not null
+        blockHeight BIGINT not null,
+        timestamp TIMESTAMP not null,
+        outputSum BIGINT unsigned
       ) """,
-      sql"""insert into transaction(transactionHash, blockHash, timestamp) values (?, ?, ?)""",
+      sql"""insert into transaction(transactionHash, blockHash, blockHeight, timestamp, outputSum) values (?, ?, ?, ?, ?)""",
       mySQL)
 
-    val inTable = new Table(sql"""
-      create table if not exists input(
-        id int(10) unsigned auto_increment not null primary key,
-        transactionHash varchar(256) not null,
-        inputScript text not null
-      ) """,
-      sql"""insert into input(transactionHash, inputScript) values (?, ?)""",
-      mySQL)
+//    val inTable = new Table(sql"""
+//      create table if not exists input(
+//        id int(10) unsigned auto_increment not null primary key,
+//        transactionHash varchar(256) not null,
+//        inputScript text not null
+//      ) """,
+//      sql"""insert into input(transactionHash, inputScript) values (?, ?)""",
+//      mySQL)
 
-    val outTable = new Table(sql"""
-      create table if not exists output(
-        id int(10) unsigned auto_increment not null primary key,
-        transactionHash varchar(256) not null,
-        outputScript text not null
-      ) """,
-      sql"""insert into output(transactionHash, outputScript) values (?, ?)""",
-      mySQL)
+//    val outTable = new Table(sql"""
+//      create table if not exists output(
+//        id int(10) unsigned auto_increment not null primary key,
+//        transactionHash varchar(256) not null,
+//        outputScript text not null
+//      ) """,
+//      sql"""insert into output(transactionHash, outputScript) values (?, ?)""",
+//      mySQL)
 
+    // scan the blockchain from first block to block height 10
+    blockchain.end(10).foreach(block => {
+      // filter out coinbases transactions
+      block.bitcoinTxs.filter(tx => tx.getInputsSum()==0).foreach(tx => {
 
-    blockchain.end(473100).foreach(block => {
-      block.bitcoinTxs.foreach(tx => {
+        txTable.insert(Seq(tx.hash.toString, block.hash.toString, block.height, convertDate(block.date), tx.getOutputsSum()))
 
-        txTable.insert(Seq(tx.hash.toString, block.hash.toString, convertDate(block.date)))
+//        tx.inputs.foreach(in => { inTable.insert(Seq(tx.hash.toString, in.inScript.toString)) })
 
-        tx.inputs.foreach(in => { inTable.insert(Seq(tx.hash.toString, in.inScript.toString)) })
-
-        tx.outputs.foreach(out => { outTable.insert(Seq(tx.hash.toString, out.outScript.toString)) })
+//        tx.outputs.foreach(out => { outTable.insert(Seq(tx.hash.toString, out.outScript.toString)) })
       })
 
-      if (block.height % 10000 == 0)
-        println(block.height)
+//      if (block.height % 10000 == 0)
+//        println(block.height)
     })
 
     txTable.close
-    inTable.close
-    outTable.close
+//    inTable.close
+//    outTable.close
 
     val totalTime = System.currentTimeMillis() / 1000 - startTime
 
